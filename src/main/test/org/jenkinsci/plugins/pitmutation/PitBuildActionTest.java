d2a73fe9d3cc3331153e368279f254c81b370a25
edward <edward@we7.com>
2013-03-14 10:45:52 +0000

    test previous successful build

--- src/main/test/org/jenkinsci/plugins/pitmutation/PitBuildActionTest.java
+++ src/main/test/org/jenkinsci/plugins/pitmutation/PitBuildActionTest.java
@@ -2,8 +2,10 @@ package org.jenkinsci.plugins.pitmutation;
 
 import org.junit.Before;
 import org.junit.Test;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.mockito.Matchers.isNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -18,25 +20,38 @@ public class PitBuildActionTest {
   private PitBuildAction action_;
   private AbstractBuild owner_;
   private AbstractBuild failedBuild_;
+  private AbstractBuild successBuild_;
   private MutationReport report_;
 
   @Before
   public void setUp() {
     failedBuild_ = mock(AbstractBuild.class);
     when(failedBuild_.getResult()).thenReturn(Result.FAILURE);
+    successBuild_ = mock(AbstractBuild.class);
+    when(successBuild_.getResult()).thenReturn(Result.SUCCESS);
     owner_ = mock(AbstractBuild.class);
     report_ = mock(MutationReport.class);
     action_ = new PitBuildAction(owner_, report_);
   }
 
   @Test
-  public void testReturnsNullIfNoPreviousBuilds() {
+  public void previousReturnsNullIfNoPreviousBuilds() {
     assertThat(action_.getPreviousAction(), nullValue());
   }
 
   @Test
-  public void testReturnsNullIfAllPreviousBuildsFailed() {
+  public void previousReturnsNullIfAllPreviousBuildsFailed() {
     when(owner_.getPreviousBuild()).thenReturn(failedBuild_);
     assertThat(action_.getPreviousAction(), nullValue());
+  }
+
+  @Test
+  public void previousReturnsLastSuccessfulBuild() {
+    PitBuildAction previousSucccessAction = mock(PitBuildAction.class);
+    when(owner_.getPreviousBuild()).thenReturn(failedBuild_);
+    when(failedBuild_.getPreviousBuild()).thenReturn(successBuild_);
+    when(successBuild_.getAction(PitBuildAction.class)).thenReturn(previousSucccessAction);
+
+    assertThat(action_.getPreviousAction(), is(previousSucccessAction));
   }
 }
