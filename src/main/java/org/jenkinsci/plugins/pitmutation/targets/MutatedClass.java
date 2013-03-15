53d04f524cf2160b6ff556c3c937801d2daef298
edward <edward@we7.com>
2013-03-15 18:15:36 +0000

    line mutation stats

--- src/main/java/org/jenkinsci/plugins/pitmutation/targets/MutatedClass.java
+++ src/main/java/org/jenkinsci/plugins/pitmutation/targets/MutatedClass.java
@@ -4,7 +4,10 @@ import org.jenkinsci.plugins.pitmutation.Mutation;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 
@@ -21,8 +24,8 @@ public class MutatedClass {
   private String package_;
   private String fileName_;
   private Collection<Mutation> mutations_;
+  private Map<Integer, Collection<Mutation>> lineMap_;
   private AbstractBuild owner_;
-
 
   public MutatedClass(AbstractBuild owner, String name, Collection<Mutation> mutations) {
     owner_ = owner;
@@ -32,6 +35,20 @@ public class MutatedClass {
     int lastDot = name.lastIndexOf('.');
     package_ = name_.substring(0, lastDot);
     fileName_ = name_.substring(lastDot + 1) + ".java.html";
+
+    lineMap_ = new HashMap<Integer, Collection<Mutation>>();
+    aggregateLines();
+  }
+
+  private void aggregateLines() {
+    for (Mutation mutation : mutations_) {
+      Collection<Mutation> lines = lineMap_.get(mutation.getLineNumber());
+      if (lines == null) {
+        lines = new ArrayList<Mutation>();
+        lineMap_.put(mutation.getLineNumber(), lines);
+      }
+      lines.add(mutation);
+    }
   }
 
   public String getName() {
@@ -47,19 +64,35 @@ public class MutatedClass {
   }
 
   public String getSourceFileContent() {
+    //can't return inner class content
+    if (fileName_.contains("$")) {
+      return "See main class.";
+    }
     try {
       return new TextFile(new File(owner_.getRootDir(), "mutation-reports/" + package_ + File.separator + fileName_)).read();
     }
     catch (IOException exception) {
-      return "Could not read source file.";
+      return "Could not read source file: " + owner_.getRootDir().getPath()
+             + "/mutation-reports/" + package_ + File.separator + fileName_ + "\n";
     }
   }
 
+  public Collection<Integer> getLineAggregations() {
+    return lineMap_.keySet();
+  }
+
+  public Collection<Mutation> getMutationsForLine(int lineNumber) {
+    return lineMap_.get(lineNumber);
+  }
+
+  public int getNumberOfMutationsOn(int lineNumber) {
+    logger.warning("Found " + lineMap_.get(lineNumber).size() + " mutations on line " + lineNumber);
+    return lineMap_.get(lineNumber).size();
+  }
   public Collection<Mutation> getMutations() {
     return mutations_;
   }
 
   private static final Logger logger = Logger.getLogger(MutationResult.class.getName());
-
 
 }
---
+++ src/main/java/org/jenkinsci/plugins/pitmutation/targets/MutatedLine.java
@@ -0,0 +1,7 @@
+package org.jenkinsci.plugins.pitmutation.targets;
+
+/**
+ * @author edward
+ */
+public class MutatedLine {
+}
